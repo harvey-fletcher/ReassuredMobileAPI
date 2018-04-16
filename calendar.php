6e681362fc7792de567ed676cf86598458c8c67a
Harvey Fletcher <harvey.fletcher1@ntlworld.com>
2018-04-16 09:32:03 +0000

    Cleaned up the calendar API to use functions in the common_functions.php file

--- calendar.php
+++ calendar.php
@@ -1,145 +1,110 @@
 <?php
-        include_once('api_settings.php');
+    //Include the API settings file
+    include_once('api_settings.php');
 
-	//The output
-	$data = array();
+    //Include the common functions file
+    include_once('common_functions.php');
 
-	if(isset($_GET['from_result'])){
-		$onwards_of = $_GET['from_result'];
-	} else {
-		$onwards_of = 0;
-	};
+    //The output
+    $data = array();
 
-        if(isset($_GET['list'])){
-		if(isset($_GET['start']) && isset($_GET['end']) ){
-			$query = "SELECT c.id, u.firstname, u.lastname, c.event_start, c.event_location, c.event_name, c.event_information FROM company_calendar c JOIN users u ON c.event_organiser=u.id WHERE c.event_start BETWEEN '". $_GET['start']  ." 00:00:01' AND '". $_GET['end']  ." 23:59:59' ORDER BY event_start ASC LIMIT ". $onwards_of .", 6";
+    if(isset($_GET['from_result'])){
+        $onwards_of = $_GET['from_result'];
+    } else {
+        $onwards_of = 0;
+    };
 
-			if($result = mysqli_query($conn, $query)){
-				while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-					$row['event_organiser'] = $row['firstname'] . " " . $row['lastname'];
-					unset($row['firstname']);
-					unset($row['lastname']);
-					array_push($data, json_encode($row, JSON_FORCE_OBJECT));
-				}
-			} else {
-				$data = array(
-	                                0 => 'Error',
-        	                        1 => 'The server encountered an error whilst querying the database.'
-	                        );
-			}
-		} else {
-			$data = array(
-                                0 => 'Error',
-       	                        1 => 'You have not specified a start and end date for the date range.'
-			);
-		}
-		//List all events between the date range
-	} else if($_GET['add']){
-		if(isset($_GET['email']) && isset($_GET['password'])){
-			$query = "SELECT team_id FROM users WHERE email='". $_GET['email']  ."' AND password='". $_GET['password']  ."'";
-			$isUser = mysqli_num_rows(mysqli_query($conn, $query));
-			$team_id = mysqli_fetch_array(mysqli_query($conn, $query))['team_id'];
+    if(isset($_GET['list'])){
+        if(isset($_GET['start']) && isset($_GET['end']) ){
+            $query = "SELECT c.id, u.firstname, u.lastname, c.event_start, c.event_location, c.event_name, c.event_information FROM company_calendar c JOIN users u ON c.event_organiser=u.id WHERE c.event_start BETWEEN '". $_GET['start']  ." 00:00:01' AND '". $_GET['end']  ." 23:59:59' ORDER BY event_start ASC LIMIT ". $onwards_of .", 6";
 
-			if($isUser == 1){
-				if(($team_id == 1) || ($team_id == 2) || ($team_id == 3)){
-					if( isset($_GET['event_name']) && isset($_GET['event_organiser']) && isset($_GET['event_start']) && isset($_GET['event_location']) && isset($_GET['event_information']) ){
-						$event_name = addslashes($_GET['event_name']);
-						$event_organiser = $_GET['event_organiser'];
-						$event_start = date("Y-m-d H:i:s", strtotime($_GET['event_start'] . '00:00:01'));
-						$event_location = $_GET['event_location'];
-						$event_information = addslashes($_GET['event_information']);
-						
-						$query = "INSERT INTO company_calendar (`event_name`,`event_organiser`,`event_start`,`event_information`,`event_location`) VALUES ('". $event_name  ."','". $event_organiser ."','". $event_start  ."','". $event_information  ."','". $event_location ."')";
-						$result = mysqli_query($conn, $query);
+            if($result = mysqli_query($conn, $query)){
+                while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+                    $row['event_organiser'] = $row['firstname'] . " " . $row['lastname'];
+                    unset($row['firstname']);
+                    unset($row['lastname']);
+                    array_push($data, json_encode($row, JSON_FORCE_OBJECT));
+                }
 
-						$send_notification = $api_hostname . "notifications.php?email=". $_GET['email']  ."&password=". $_GET['password']  ."&type=everyone&notify=calendar&timeframe=future";
-						$send_notification = shell_exec("curl '". $send_notification."'");
+                //Output the data using the function in common_functions.php
+                stdout($data);
 
-						file_put_contents('data_received.txt', mysqli_affected_rows($conn) . " Have been inserted");
+            } else {
+		stdout(array('Error', 'The server encountered an error whilst querying the database.'));
+            }
+        } else {
+            stdout(array('Error', 'You have not specified a start and end date for the date range.'));
+        }
+    } else if($_GET['add']){
+        if(isset($_GET['email']) && isset($_GET['password'])){
+            //Auth the user here.
+            auth($_GET['email'], $_GET['password']);
+            $team_id = $GLOBALS['USER']['team_id'];
 
-						$data = array(
-							'status' => '200',
-							'reason' => 'Event received'
-						);
-					} else {
-						$data = array(
-							'status' => '500',
-							'reason' => 'You have not specified enough fields. Required fields are: event_name, event_location, event_organiser, event_start, event_end, event_information'
-						);
-					}
-				} else {
-					$data = array(
-                                                'status' => '403',
-                                                'reason' => 'Your user group cannot perform this action'
-                                        );
-				}
-			} else {
-				$data = array(
-                                        'status' => '403',
-                                	'reason' => 'Username or password incorrect'
-                                );
-			}
-		} else {
-			$data = array(
-                                'status' => 'Error',
-                                'reason' => 'You have not given username and password'
+            if(($team_id == 1) || ($team_id == 2) || ($team_id == 3)){
+                if( isset($_GET['event_name']) && isset($_GET['event_organiser']) && isset($_GET['event_start']) && isset($_GET['event_location']) && isset($_GET['event_information']) ){
+                    $event_name = addslashes($_GET['event_name']);
+                    $event_organiser = $_GET['event_organiser'];
+                    $event_start = date("Y-m-d H:i:s", strtotime($_GET['event_start'] . '00:00:01'));
+                    $event_location = $_GET['event_location'];
+                    $event_information = addslashes($_GET['event_information']);
+
+                    //Insert the row into the database
+                    $query = "INSERT INTO company_calendar (`event_name`,`event_organiser`,`event_start`,`event_information`,`event_location`) VALUES ('". $event_name  ."','". $event_organiser ."','". $event_start  ."','". $event_information  ."','". $event_location ."')";
+                    $result = mysqli_query($GLOBALS['conn'], $query);
+
+                    //This is the data we want to send in the notification
+                    $data = array(
+                            "data" => array(
+                                    "notification_type" => "calendar",
+                                ),
                         );
-		}
-	} else if($_GET['delete']){
-		if(isset($_GET['email']) && isset($_GET['password'])){
-			$query = "SELECT team_id FROM users WHERE email regexp '[[:<:]]". $_GET['email']  ."[[:>:]]' AND password regexp '[[:<:]]". $_GET['password']  ."[[:>:]]'" ;
-			$isUser = mysqli_num_rows(mysqli_query($conn, $query));
-			$team_id = mysqli_fetch_array(mysqli_query($conn, $query))['team_id'];
 
-			if($isUser == 1){
-				if(($team_id == 1) || ($team_id == 2) || ($team_id == 3)){
-					if(isset($_GET['id'])){
-					
-						$query = "DELETE FROM company_calendar WHERE id=" . $_GET['id'];
-						$execute = mysqli_query($conn, $query);
-						$result = mysqli_affected_rows($conn);
-					
-						if($result != 0){
-							$data = array(
-								'status' => '200',
-								'reason' => 'Deleted ' . $result . ' event from the company calendar'
-							);
-						} else {
-							$data = array(
-                                	                        'status' => '200',
-                                        	                'reason' => 'Done, there were no rows to delete'
-	                                                );
-						};
-					} else {
-						$data = array(
-							'status' => '500',
-							'reason' => 'Please provide an event ID'
-						);
-					}
-				} else {
-					$data = array(
-						'status' => '403',
-						'reason' => 'Your user group cannot perform this action'
-					);
-				}
-			} else {
-				$data = array(
-					'status' => '403',
-					'reason' => 'Username or password incorrect.'
-				);
-			}
-		} else {
-			$data = array(
-                                'status' => 'Error',
-   				'reason' => 'You have not given username and password'
-			);
-		}
-	} else {
-		$data = array(
-			0 => 'Error',
-			1 => 'You have not specified an API mode. API modes: list, create, delete'
-		);
-	}
-		
-	echo json_encode($data);
+                    //This function is located in common_functions.php, it sends the notification
+                    sendFCM(array(1), $data);
+
+                    //Finish
+                    stdout(array('status' => '200', 'reason' => 'Event received'));
+                } else {
+                    //Finish, not enough fields.
+                    stdout(array('status' => '400', 'reason' => 'You have not specified enough fields.'));
+                }
+            } else {
+                //Finish, permissions error, user group
+                stdout(array('status' => '403', 'reason' => 'Your user group cannot perform this action'));
+            }
+        }
+    } else if($_GET['delete']){
+        if(isset($_GET['email']) && isset($_GET['password'])){
+            //Auth the user using common_functions.php auth()
+            auth($_GET['email'], $_GET['password']);
+            $team_id = $GLOBALS['USER']['team_id'];
+
+            //Does the user have the right permissions to delete an event from the calendar?
+            if(($team_id == 1) || ($team_id == 2) || ($team_id == 3)){
+                if(isset($_GET['id'])){
+                    $query = "DELETE FROM company_calendar WHERE id=" . $_GET['id'];
+                    $execute = mysqli_query($conn, $query);
+                    $result = mysqli_affected_rows($conn);
+
+                    if($result != 0){
+                        //Finish, success, row deleted
+                        stdout(array('status' => '200', 'reason' => 'Deleted ' . $result . ' event from the company calendar'));
+                    } else {
+                        //Finish, success, rows already gone.
+                        stdout(array('status' => '200', 'reason' => 'Done, there were no rows to delete'));
+                    };
+                } else {
+                    //Finish, bad request, no ID
+                    stdout(array('status' => '400', 'reason' => 'Please provide an event ID'));
+                }
+            }
+        } else {
+            //Finish, no credentials
+            stdout(array('status' => '403', 'reason' => 'You have not given username and password'));
+        }
+    } else {
+        //Finish, no API mode
+        stdout(array('status'=>'400', 'reason' => 'You have not specified an API mode. API modes: list, create, delete'));
+    }
 ?>
--- common_functions.php
+++ common_functions.php
@@ -23,6 +23,58 @@
         $GLOBALS['USER']['status'] = 200;
     }
 
+    //This function sends a message to the FCM service and sends it to the specified $tokens
+    function sendFCM($group, $data){
+        /**
+            Here is a list of groups. Groups are specified by an array.
+
+            1 - This group sends to everyone. `$group = array(1)`
+            2 - This group is everyone in a team. `$group = array(2, $team_id)`
+            3 - This group sends to a specific user ID. `$group = array(3, $user_id)`
+            4 - This group sends to everyone at a specified location. `$group = array(4, $location_id)`
+        **/
+
+        //Each different group requires a different query.
+        if($group[0] == 1)$query = "SELECT * FROM application_tokens";
+        if($group[0] == 2)$query = "SELECT a.* FROM application_tokens a JOIN users u ON u.id=a.user_id WHERE u.team_id=" . $group[1];
+        if($group[0] == 3)$query = "SELECT * FROM application_tokens WHERE user_id=" . $group[1];
+        if($group[0] == 4)$query = "SELECT a.* FROM application_tokens a JOIN users u ON u.id=a.user_id WHERE a.location_id=" . $group[1];
+
+        //Execute the query
+        $mysqli_tokens = mysqli_query($GLOBALS['conn'], $query);
+
+        //This will be an array of tokens
+        $tokens = array();
+
+        //For each result in the resultset, add the token on to the $tokens array
+        while($row = mysqli_fetch_array($mysqli_tokens, MYSQLI_ASSOC)){
+            array_push($tokens, $row['application_token']);
+        }
+
+        //Add the registration IDs to the data
+        $data["registration_ids"] = $tokens;
+
+        //Build up the post request
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+                CURLOPT_URL => $GLOBALS['external_endpoints']['fcm'],
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "POST",
+                CURLOPT_POSTFIELDS => json_encode($data),
+                CURLOPT_HTTPHEADER => array(
+                        "Authorization: key=" . $GLOBALS['external_keys']['fcm'],
+                        "Cache-Control: no-cache",
+                        "Content-Type: application/json",
+                    ),
+            ));
+
+        //Make the post request to the FCM service
+        curl_exec($curl);
+    }
+
     //Send an email in HTML format
     function HTMLMailer($to, $body, $subject, $sender){
         //Set up some headers for the mail.
