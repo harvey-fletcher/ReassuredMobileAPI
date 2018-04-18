f980e2343e2eed8ecbcc5328ef167b642ca7735c
Harvey Fletcher <harvey.fletcher1@ntlworld.com>
2018-04-18 08:12:39 +0000

    Merged in the branches for release, added the user_messages change to the setup SQL

--- Database_Setup/setup.sql
+++ Database_Setup/setup.sql
@@ -142,3 +142,13 @@ CREATE TABLE pending_actions(
   `affects_user_id` INT(10) NOT NULL,
   PRIMARY KEY(id)
 );
+
+/*This script will create a table to store user to user messaging*/
+CREATE TABLE user_messages(
+  id INT(10) AUTO_INCREMENT,
+  from_user_id INT(10) NOT NULL,
+  to_user_id INT(10) NOT NULL,
+  sent_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  message_body TEXT(9999) NOT NULL,
+  PRIMARY KEY(id)
+);
